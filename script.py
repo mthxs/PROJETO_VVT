4b55797355d6ff4dfe1b9586e27d37e1765d918b
Matheus Guilherme <141871576+mthxs@users.noreply.github.com>
2025-04-26 15:15:02 -0300

    tarefa para dia 29/4

---
+++ script.py
@@ -0,0 +1,14 @@
+import requests
+
+def fetch_cep_data(cep):
+    url = f"https://viacep.com.br/ws/{cep}/json/"
+    response = requests.get(url)
+    if response.status_code == 200:
+        print("Resposta da API ViaCEP:")
+        print(response.json())
+    else:
+        print(f"Erro ao acessar API: {response.status_code}")
+
+if __name__ == "__main__":
+    cep = "80610905" 
+    fetch_cep_data(cep)
